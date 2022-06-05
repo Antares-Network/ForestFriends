03713efefa3c7d7726f816198a7dec28da379693
Nate Goldsborough <nathen418@playantares.com>
2022-06-05 13:49:00 -0500

    fixed naming convention error

--- boot_func/dbConnected.ts
+++ boot_func/dbConnected.ts
@@ -4,11 +4,11 @@ import { Client } from "discord.js";
 
 async function event(client: Client) {
     console.log(chalk.green("Connected to MongoDB"));
-    // Print some bot stats
+    //Print some bot stats
     console.log(`${chalk.yellow("I am in")} ${chalk.green((await client.guilds.fetch()).size)} ${chalk.yellow("servers")}`);
     try {
-        const gate = await botModel.findOne({ BOT_ID: process.env.BOT_ID });
-        console.log(`${chalk.yellow("I am being used by")} ${chalk.green(gate.TOTAL_USERS)} ${chalk.yellow("users")}`);
+        const doc = await botModel.findOne({ BOT_ID: process.env.BOT_ID });
+        console.log(`${chalk.yellow("I am being used by")} ${chalk.green(doc.TOTAL_USERS)} ${chalk.yellow("users")}`);
     } catch (e) {
         console.log(e);
     }
